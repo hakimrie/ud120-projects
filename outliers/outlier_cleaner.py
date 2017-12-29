4411d30234e3a4df567b5e324440661dc19ea1c5
Muhammad Hakim Asy'ari <hakimrie@gmail.com>
2017-12-29 07:05:43 +0700

    quiz 8.13 score after cleaning

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -14,7 +14,13 @@ def outlierCleaner(predictions, ages, net_worths):
     cleaned_data = []
 
     ### your code goes here
-
+    num_data = len(predictions)
+    for x in range(0, num_data):
+        age = ages[x][0]
+        net_worth = net_worths[x][0]
+        prediction = predictions[x][0]
+        cleaned_data.append((age, net_worth, prediction - net_worth))
     
-    return cleaned_data
+    cleaned_data = sorted(cleaned_data, key=lambda tdata: tdata[2])
+    return cleaned_data[:-9]
 
--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -58,8 +58,6 @@ except NameError:
 
 
 
-
-
 ### only run this code if cleaned_data is returning data
 if len(cleaned_data) > 0:
     ages, net_worths, errors = zip(*cleaned_data)
@@ -69,6 +67,8 @@ if len(cleaned_data) > 0:
     ### refit your cleaned data!
     try:
         reg.fit(ages, net_worths)
+        print "new slope: ", reg.coef_
+        print "new score: ", reg.score(ages_test, net_worths_test)
         plt.plot(ages, reg.predict(ages), color="blue")
     except NameError:
         print "you don't seem to have regression imported/created,"
