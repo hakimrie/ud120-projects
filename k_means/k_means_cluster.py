3c0229cea462fd8edbbca67d9ba1321c62eec42c
Muhammad Hakim Asy'ari <hakimrie@gmail.com>
2017-12-31 18:46:34 +0700

    solve mini project lesson 9 clustering

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -43,13 +43,34 @@ data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r")
 ### there's an outlier--remove it! 
 data_dict.pop("TOTAL", 0)
 
+max_exercised_stock_options = 0
+min_exercised_stock_options = sys.maxint
+max_salary = 0
+min_salary = sys.maxint
+for key in data_dict:
+    exercised_stock_options = data_dict[key]['exercised_stock_options']
+    salary = data_dict[key]['salary']
+    if (exercised_stock_options != 'NaN'):
+        if exercised_stock_options > max_exercised_stock_options:
+            max_exercised_stock_options = exercised_stock_options
+        if exercised_stock_options < min_exercised_stock_options:
+            min_exercised_stock_options = exercised_stock_options
+    
+    if salary != 'NaN':
+        if max_salary < salary:
+            max_salary = salary
+        if min_salary > salary:
+            min_salary = salary
 
+print "min excercised stock options: %s max = %s" % (min_exercised_stock_options, max_exercised_stock_options)
+print "min salary : %s max = %s" % (min_salary, max_salary)
 ### the input features we want to use 
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
+features_list = [poi, feature_1, feature_2, feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
@@ -58,19 +79,19 @@ poi, finance_features = targetFeatureSplit( data )
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
+for f1, f2, _ in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
-kmeans = KMeans(n_clusters= 2).fit(finance_features)
+kmeans = KMeans(n_clusters= 3).fit(finance_features)
 pred = kmeans.predict(finance_features)
 
 
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
 try:
-    Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
+    Draw(pred, finance_features, poi, mark_poi=False, name="clusters_3_features.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
     print "no predictions object named pred found, no clusters to plot"
